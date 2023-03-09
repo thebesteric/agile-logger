23b793cdd36be6f3ccd1b25940df7c69b123f9ac
eric <whatisjava@hotmail.com>
2023-03-09 18:56:44 +0800

    :art: perf: @RewriteField support RequestLog

--- agile-logger-spring/src/main/java/io/github/thebesteric/framework/agile/logger/spring/processor/request/AbstractRequestLoggerProcessor.java
+++ agile-logger-spring/src/main/java/io/github/thebesteric/framework/agile/logger/spring/processor/request/AbstractRequestLoggerProcessor.java
@@ -1,5 +1,8 @@
 package io.github.thebesteric.framework.agile.logger.spring.processor.request;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.*;
+import io.github.thebesteric.framework.agile.logger.commons.utils.CollectionUtils;
 import io.github.thebesteric.framework.agile.logger.commons.utils.DurationWatcher;
 import io.github.thebesteric.framework.agile.logger.commons.utils.JsonUtils;
 import io.github.thebesteric.framework.agile.logger.commons.utils.LoggerPrinter;
@@ -7,12 +10,16 @@ import io.github.thebesteric.framework.agile.logger.core.domain.ExecuteInfo;
 import io.github.thebesteric.framework.agile.logger.core.domain.InvokeLog;
 import io.github.thebesteric.framework.agile.logger.spring.domain.RequestLog;
 import io.github.thebesteric.framework.agile.logger.spring.processor.RequestLoggerProcessor;
+import io.github.thebesteric.framework.agile.logger.spring.wrapper.AgileLoggerContext;
 import io.github.thebesteric.framework.agile.logger.spring.wrapper.AgileLoggerRequestWrapper;
 import io.github.thebesteric.framework.agile.logger.spring.wrapper.AgileLoggerResponseWrapper;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * AbstractRequestLoggerProcessor
@@ -41,6 +48,9 @@ public abstract class AbstractRequestLoggerProcessor implements RequestLoggerPro
             }
             requestLog.setExecuteInfo(new ExecuteInfo(method, null, duration));
         }
+
+        rewriteField(requestLog);
+
         return doAfterProcessor(requestLog);
     }
 
@@ -51,4 +61,58 @@ public abstract class AbstractRequestLoggerProcessor implements RequestLoggerPro
      * @return RequestLog
      */
     public abstract RequestLog doAfterProcessor(RequestLog requestLog);
+
+    private void rewriteField(RequestLog requestLog) {
+        Map<Field, Object> rewriteFields = AgileLoggerContext.getRewriteFields();
+        if (CollectionUtils.isEmpty(rewriteFields)) {
+            return;
+        }
+        doRewriteField(requestLog.getResult(), rewriteFields);
+    }
+
+    public void doRewriteField(Object result, Map<Field, Object> rewriteFields) {
+        try {
+            if (result instanceof ObjectNode) {
+                ObjectNode objectNode = (ObjectNode) result;
+
+                Iterator<String> fieldNames = objectNode.fieldNames();
+                while (fieldNames.hasNext()) {
+                    String fieldName = fieldNames.next();
+                    JsonNode jsonNode = objectNode.get(fieldName);
+                    // POJO
+                    if (jsonNode instanceof ObjectNode) {
+                        doRewriteField(jsonNode, rewriteFields);
+                    }
+                    // List or Array or Map
+                    else if (jsonNode instanceof ArrayNode) {
+                        ArrayNode arrayNode = (ArrayNode) jsonNode;
+                        for (JsonNode node : arrayNode) {
+                            doRewriteField(node, rewriteFields);
+                        }
+                    }
+
+                    if (jsonNode instanceof ValueNode) {
+                        Map.Entry<Field, Object> entry = rewriteFields.entrySet().stream()
+                                .filter(e -> e.getKey().getName().equals(fieldName)).findFirst().orElse(null);
+                        if (entry != null) {
+                            if (jsonNode instanceof IntNode || jsonNode instanceof BigIntegerNode || jsonNode instanceof ShortNode) {
+                                objectNode.put(fieldName, (int) entry.getValue());
+                            } else if (jsonNode instanceof LongNode) {
+                                objectNode.put(fieldName, (long) entry.getValue());
+                            } else if (jsonNode instanceof FloatNode) {
+                                objectNode.put(fieldName, (float) entry.getValue());
+                            } else if (jsonNode instanceof DoubleNode) {
+                                objectNode.put(fieldName, (double) entry.getValue());
+                            } else if (jsonNode instanceof BooleanNode) {
+                                objectNode.put(fieldName, (boolean) entry.getValue());
+                            } else {
+                                objectNode.put(fieldName, String.valueOf(entry.getValue()));
+                            }
+                        }
+                    }
+                }
+            }
+        } catch (Exception ignored) {
+        }
+    }
 }
